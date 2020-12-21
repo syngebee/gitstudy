0e6255059373b3ec7e74e445075a764b6aa0ff71
cyc <65350480+syngebee@users.noreply.github.com>
2020-12-21 23:44:34 +0800

    Update GitInit.java

--- src/main/java/com/itheima/GitInit.java
+++ src/main/java/com/itheima/GitInit.java
@@ -2,6 +2,7 @@ package com.itheima;
 
 public class GitInit {
     public static void main(String[] args) {
-        System.out.println("试试");
+        int a = 1;
+        System.out.println("变更");
     }
 }
